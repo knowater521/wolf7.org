483add0e98351b779e0e5146d5086e9ce1def47a
A wolf from the north <rzxwang@users.noreply.github.com>
2017-12-20 10:17:08 +0800

    Update apache_cleck_log.sh

--- Shell/apache_cleck_log.sh
+++ Shell/apache_cleck_log.sh
@@ -2,12 +2,12 @@
 # Deniss.wang
 # Check 4xx and 5xx in logs and send Email
 # Add own filter url file list and Assign error url directory 
-# Date: 2017-11-20
+# Date: 2017-11-20 00:30
 #
-#log_file='/var/log/httpd/access_log'
-#now_time=`date -d "2 minute ago" +%d/%b/%Y:%H:%M`
-log_file='./access_log'
-now_time='18/Dec/2017:09:41'
+log_file='/var/log/httpd/access_log'
+now_time=`date -d "2 minute ago" +%d/%b/%Y:%H:%M`
+#log_file='./access_log'
+#now_time='19/Dec/2017:23:23'
 project='example'
 email_add='deniss.wang@example.com'
 statuscode_list=(404 401 403 400 500 501 503 504)
@@ -21,17 +21,14 @@ web_code_sendemail(){
         do
         statuscode=`tail -n 50 $log_file|grep -w $dir_list|grep -w $code_list|egrep -v $web_filter_list|awk 'END {print}'|awk '{print $10}'`
         status_time=`tail -n 50 $log_file|grep -w $dir_list|grep -w $code_list|egrep -v $web_filter_list|awk  'END {print}'|awk -F' ' '{print $5}'|awk -F'[' '{print $2}'|cut -c 1-17`
-        detail=`tail -n 50 $log_file|grep -w $code_list|grep -w $dir_list|egrep -v $web_filter_list |awk 'NR==1 {print}'`
-        check_dir_list=`tail -n 50 $log_file|grep -w $code_list|grep -w $dir_list|awk '{print $8}'|awk -F'/' '{print $2}'`
+        detail=`tail -n 50 $log_file|grep -w $dir_list|grep -w $code_list| egrep -v $web_filter_list|awk 'END {print}'`
+        check_dir_list=`tail -n 50 $log_file|grep -w $dir_list|grep -w $code_list|egrep -v $web_filter_list|awk 'END {print}'|awk '{print $8}' |awk -F'/' '{print $2}'`
             if [[ $dir_list == $check_dir_list && $statuscode == $code_list &&  $status_time == $now_time ]];then
-            # 打印code发送Email
-                echo "Los Detail: $detail " #| mail -s "$code_list error of $project" $email_add
-                #echo $statuscode=$code_list,$status_time=$now_time,$dir_list=$check_dir_list
+            # print log detail send Email
+                echo "Los Detail: $detail " | mail -s "$code_list error of $project" $email_add
             else
-            #date -d "2 minute ago" +%d/%b/%Y:%H:%M >> /tmp/status_code.txt
                 echo "no error in logs" > /dev/null 2>&1
             fi
-        continue
         done
     done
 }
