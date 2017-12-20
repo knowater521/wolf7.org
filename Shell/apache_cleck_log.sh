e594c1cf71d0236fad6f3b2200d59b5949fb9d63
A wolf from the north <rzxwang@users.noreply.github.com>
2017-12-20 11:54:17 +0800

    Update apache_cleck_log.sh

--- Shell/apache_cleck_log.sh
+++ Shell/apache_cleck_log.sh
@@ -4,14 +4,14 @@
 # Add own filter url file list and Assign error url directory 
 # Date: 2017-11-20 00:30
 #
-log_file='/var/log/httpd/access_log'
-now_time=`date -d "2 minute ago" +%d/%b/%Y:%H:%M`
-#log_file='./access_log'
-#now_time='19/Dec/2017:23:23'
+#log_file='/var/log/httpd/access_log'
+#now_time=`date -d "2 minute ago" +%d/%b/%Y:%H:%M`
+log_file='./access_log'
+now_time='20/Dec/2017:03:33'
 project='example'
 email_add='deniss.wang@example.com'
 statuscode_list=(404 401 403 400 500 501 503 504)
-web_filter_list='robot.txt|favicon.ico|testproxy.php|device_description.xml'
+web_filter_list='robot.txt|favicon.ico|testproxy.php|device_description.xml|location.default.css'
 directory_list=(agency advertiser manager)
 # If 4xx/5xx and sendEmail function
 web_code_sendemail(){
@@ -21,11 +21,12 @@ web_code_sendemail(){
         do
         statuscode=`tail -n 50 $log_file|grep -w $dir_list|grep -w $code_list|egrep -v $web_filter_list|awk 'END {print}'|awk '{print $10}'`
         status_time=`tail -n 50 $log_file|grep -w $dir_list|grep -w $code_list|egrep -v $web_filter_list|awk  'END {print}'|awk -F' ' '{print $5}'|awk -F'[' '{print $2}'|cut -c 1-17`
-        detail=`tail -n 50 $log_file|grep -w $dir_list|grep -w $code_list| egrep -v $web_filter_list|awk 'END {print}'`
+        detail=`tail -n 50 $log_file|grep -w $dir_list|grep -w $code_list| egrep -v $web_filter_list #|awk 'END {print}'`
         check_dir_list=`tail -n 50 $log_file|grep -w $dir_list|grep -w $code_list|egrep -v $web_filter_list|awk 'END {print}'|awk '{print $8}' |awk -F'/' '{print $2}'`
             if [[ $dir_list == $check_dir_list && $statuscode == $code_list &&  $status_time == $now_time ]];then
-            # print log detail send to Email
-                echo "Logs Detail: $detail " | mail -s "$code_list error of $project" $email_add
+            #if [[ $statuscode == $code_list &&  $status_time == $now_time ]];then
+            # print logs detail send to email
+                echo "Los Detail: $detail " #| mail -s "$code_list error of $project" $email_add
             else
                 echo "no error in logs" > /dev/null 2>&1
             fi
