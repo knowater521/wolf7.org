f4a73959648ce07b10b7087358f56c8c90369dbe
A wolf from the north <rzxwang@users.noreply.github.com>
2017-12-27 10:47:20 +0800

    Update SendEmail.py

--- Python/SendEmail.py
+++ Python/SendEmail.py
@@ -4,13 +4,15 @@
 # Send Email for office365 Email
 import smtplib  
 from email.mime.text import MIMEText  # 引入smtplib和MIMEText
+to_email_list = ['deniss.wang@example.com','abc@example.com']
 
 def SendMail(Email):
     host = 'smtp.office365.com'  # 设置发件服务器地址
     port = 587  # 设置发件服务器端口号。注意，这里有SSL和非SSL两种形式 25 465 587
     sender = 'wolf7@example.com'  # 设置发件邮箱，一定要自己注册的邮箱
     pwd = '1234567'  # 设置发件邮箱的密码，等会登陆会用到
-    receiver = 'deniss.wang@palmax.com' # 设置邮件接收人，这里是我的公司邮箱
+    # receiver = 'deniss.wang@palmax.com' # 设置单个邮件接收人
+    receiver = ','.join(to_email_list) # 设置群发邮件
     body = '<h1>Hi,How are you.</h1><h3>Deniss.wang</h3>' # 设置邮件正文，这里是支持HTML的
 
     msg = MIMEText(body, 'html') # 设置正文为符合邮件格式的HTML内容
