3bd3c1e0b65d0f70dd5ca81c8cc7277fd3dd8147
A wolf from the north <rzxwang@users.noreply.github.com>
2017-12-28 10:35:26 +0800

    Update Mass_SendEmail-demo.py

--- Python/Mass_SendEmail-demo.py
+++ Python/Mass_SendEmail-demo.py
@@ -10,13 +10,13 @@ def _format_addr(s):
     name, addr = parseaddr(s)
     return formataddr((Header(name, 'utf-8').encode(), addr))
 def send_mail(title, content):
-    from_user = '发件人'
+    from_user = '138@139.com'
     to_user = '收件人'
-    from_addr = '139@139.com'
+    from_addr = '138@139.com'
     password = '******'
     to_addr = 'deniss.wang@gmail.com,9935226@qq.com'
     smtp_server = 'smtp.139.com',
-    msg = MIMEText(content, 'plain', 'utf-8')
+    msg = MIMEText(content, 'html', 'utf-8')
     msg['From'] = _format_addr(from_user+' <%s>' % from_addr)
     msg['To'] = _format_addr(to_user+' <%s>' % to_addr)
     msg['Subject'] = Header(title, 'utf-8').encode()
@@ -28,6 +28,6 @@ def send_mail(title, content):
     server.quit()
 if __name__=="__main__":
     subject = '测试'
-    body = '邮件正文'
+    body = '<h2>内容:</h2><h4>Hello,World!</h4>'
     send_mail(subject, body)
 
