e0e45639736403554ebdc1a7c7c8b6adfd8944c2
Wolf Emperor <9935226@qq.com>
2019-07-02 00:18:17 +0800

    Update aws_daily_alert.py

--- Python/aws_daily_alert.py
+++ Python/aws_daily_alert.py
@@ -12,8 +12,8 @@ json_demo1 = """
             "Estimated": true,
             "Total": {},
             "TimePeriod": {
-                "Start": "2017-06-30",
-                "End": "2017-07-01"
+                "Start": "2019-06-30",
+                "End": "2019-07-01"
             },
             "Groups": [
                 {
@@ -56,11 +56,10 @@ EC2_OTHER_AMOUNT = jsonData['ResultsByTime'][0]['Groups'][0]['Metrics']['Unblend
 EC2_KEYS = jsonData['ResultsByTime'][0]['Groups'][1]['Keys'][0]
 EC2_AMOUNT = jsonData['ResultsByTime'][0]['Groups'][1]['Metrics']['UnblendedCost']['Amount']
 if EC2_OTHER_KEYS == "EC2 - Other":
-	if float(EC2_OTHER_AMOUNT) > 1:
-		print ("Service: %s Bill above 1" % (EC2_OTHER_KEYS))
+	if float(EC2_OTHER_AMOUNT) > 2:
+		print ("Service: %s Bill above 2" % (EC2_OTHER_KEYS))
 		print (EC2_OTHER_AMOUNT)
 	elif (EC2_KEYS) == "Amazon Elastic Compute Cloud - Compute":
-	if float(EC2_AMOUNT) > 1:
-		print ("Service: %s Bill above 1" % (EC2_KEYS))
-		print (EC2_AMOUNT)
-	
+		if float(EC2_AMOUNT) > 3:
+			print ("Service: %s Bill above 3" % (EC2_KEYS))
+			print (EC2_AMOUNT)
