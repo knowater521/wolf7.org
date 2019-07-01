13c4a3d9fde8df23544f6968bcc8ad3f16afc704
Wolf Emperor <9935226@qq.com>
2019-07-02 00:38:49 +0800

    Update aws_daily_alert.py

--- Python/aws_daily_alert.py
+++ Python/aws_daily_alert.py
@@ -51,15 +51,15 @@ json_demo1 = """
 """
 jsonData = json.loads(json_demo1)
 #print jsonData
+START_TIME = jsonData['ResultsByTime'][0]['TimePeriod']['Start']
+END_TIME = jsonData['ResultsByTime'][0]['TimePeriod']['End']
 EC2_OTHER_KEYS = jsonData['ResultsByTime'][0]['Groups'][0]['Keys'][0]
 EC2_OTHER_AMOUNT = jsonData['ResultsByTime'][0]['Groups'][0]['Metrics']['UnblendedCost']['Amount']
 EC2_KEYS = jsonData['ResultsByTime'][0]['Groups'][1]['Keys'][0]
 EC2_AMOUNT = jsonData['ResultsByTime'][0]['Groups'][1]['Metrics']['UnblendedCost']['Amount']
 if EC2_OTHER_KEYS == "EC2 - Other":
-	if float(EC2_OTHER_AMOUNT) > 2:
-		print ("Service: %s Bill above 2" % (EC2_OTHER_KEYS))
-		print (EC2_OTHER_AMOUNT)
-	elif (EC2_KEYS) == "Amazon Elastic Compute Cloud - Compute":
-		if float(EC2_AMOUNT) > 3:
-			print ("Service: %s Bill above 3" % (EC2_KEYS))
-			print (EC2_AMOUNT)
+	if float(EC2_OTHER_AMOUNT) > 1:
+		print ("Service: %s, Start_time: %s End_time: %s, Billing Detail: %s " % (EC2_OTHER_KEYS,START_TIME,END_TIME,EC2_OTHER_AMOUNT))
+if (EC2_KEYS) == "Amazon Elastic Compute Cloud - Compute":
+	if float(EC2_AMOUNT) > 6:
+		print ("Service: %s, Start_time: %s End_time: %s, Billing Detail %s" % (EC2_KEYS,START_TIME,END_TIME,EC2_AMOUNT))
